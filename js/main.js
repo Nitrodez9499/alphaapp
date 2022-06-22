dcf2af933c7994a4ab89a04366e5374f85580bfb
Maximilian Iniewicz <maximilian@iniewi.cz>
2022-06-22 18:21:12 -0400

    Completed kills/deaths, added and styled Gunner

--- js/main.js
+++ js/main.js
@@ -5,32 +5,46 @@ function fetchMaxScores() {
 
     const hiveJsonResponse = hiveResult.then(response => response.json());
 
-    hiveJsonResponse.then(response => showKills(response.kills));
+    hiveJsonResponse.then(response => showStat(response.kills, 'maxScore'));
 
-    hiveJsonResponse.then(response => showDeaths(response.deaths));
+    hiveJsonResponse.then(response => showStat(response.deaths, 'maxLoss'));
 
-    hiveJsonResponse.then(response => killDeath(response.kills, response.deaths));
+    hiveJsonResponse.then(response => killDeath(response.kills, response.deaths, 'maxKD'));
 }
 
-function showKills(kills) {
-    const scoreElement = document.getElementById("maxScore");
-    scoreElement.textContent = kills;
+function fetchGunScores() {
+    const hiveUri = "https://api.playhive.com/v0/game/all/wars/SpaceFox0444";
+
+    const hiveResult = fetch(hiveUri);
+
+    const hiveJsonResponse = hiveResult.then(response => response.json());
+
+    hiveJsonResponse.then(response => showStat(response.kills, 'gunScore'));
+
+    hiveJsonResponse.then(response => showStat(response.deaths, 'gunLoss'));
+
+    hiveJsonResponse.then(response => killDeath(response.kills, response.deaths, 'elementID'));
 }
 
-function showDeaths(deaths) {
-    const scoreElement = document.getElementById("maxLoss");
-    scoreElement.textContent = deaths;
+function showStat(statistic, elementID) {
+    const scoreElement = document.getElementById(elementID);
+    scoreElement.textContent = statistic;
 }
 
-function killDeath(kills, deaths) {
+function killDeath(kills, deaths, elementID) {
     var a = kills;
     var b = deaths;
     var c = a / b - 1;
+    if(c < 0) {
+        c = 0;
+    }
     var d = c * 100;
-    var e = Math.round(d)
-    const scoreElement = document.getElementById("maxKD");
-    scoreElement.textContent = e;
+    var e = Math.round(d);
+
+    showStat(e, elementID);
 }
+
 /*
     JSON = JavaScript Object Notation
+    DRY Principal - Don't Reapeat Yourself
 */
